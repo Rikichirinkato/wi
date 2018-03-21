466a004460f47bf528cdf7527b4bfc890e67302a
Rikichirinkato <roman28krasko@gmail.com>
2018-03-21 22:26:12 +0200

    redux-start

--- src/App.js
+++ src/App.js
@@ -1,61 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      allAmounts: [],
-      stuff: []
+  adding() {    
+      let newBuy = this.buyInput.value;
+      let newCost = parseFloat(this.costInput.value);
+      let buyObj = {
+          buy: newBuy,
+          cost: newCost
       }
+      this.props.onAddBuy(buyObj);
+      this.buyInput.value = '';
+      this.costInput.value = ''; 
   }
-  adding(){
-      let name = document.getElementById('name').value;
-      name = name.slice(0,1).toUpperCase() + name.slice(1)
-      let amount = parseFloat(document.getElementById('amount').value);
-      if(name === ''){
-        document.getElementById('name').style.borderColor = 'red';
-        document.getElementById('amount').style.borderColor = 'red';
-        return;
-      }
-      else{
-        document.getElementById('name').style.borderColor = '#3192af';
-        document.getElementById('amount').style.borderColor = '#3192af';
-      }
-      
-      let newAllAmounts = this.state.allAmounts;
-      newAllAmounts.push(amount);
-      
-      let newStuff = this.state.stuff;
-      newStuff.push({ buy:`${name}`, price: `${amount}`} );
-      
-      this.setState({allAmounts: newAllAmounts});
-      this.setState({stuff: newStuff});
-                     
-      document.getElementById('name').value = '';
-      document.getElementById('amount').value = '';
-      let total = newAllAmounts.reduce((x,y) => {
-        return x+y;
+  
+  totalCost(){
+      let result = this.props.currentStore.reduce( (prev, acum) => {
+          return prev.cost + acum.cost
       });
-      document.getElementById('result').innerHTML = '$' + total;
-  }
-  sort() {
-      let buyName = [];
-      this.state.stuff.map( item => buyName.push(item.buy));
-      buyName.sort();
-      let newStuff = [];
-      buyName.forEach( item => {
-          this.state.stuff.forEach( buy => {
-              if(item !== buy.buy){
-                  return
-              } else { 
-                newStuff.push({ buy:`${buy.buy}`, price: `${buy.price}`} );
-              }
-          })
-      });
-      this.setState({ stuff: newStuff })
+      console.log(result)
   }
     
+  sort() {
+      console.log(this.props.currentStore); 
+  }
   render() {
     return (
         <article className="app">
@@ -75,10 +44,12 @@ class App extends Component {
               </p>
               <p>Amount</p>
           </div>
-          <div className="list">{this.state.stuff.map( item => { return (<div className="cost" key={`${Math.random()*10}`}><span>{item.buy}</span><span>{item.price}</span></div>) })}</div>
+          <div className="list">
+              {this.props.currentStore.map(el => <div className="cost" key={Math.random()}><span>{el.buy}</span><span>{el.cost}</span></div> )}
+          </div>
           <form>
-            <input type="text" name="name" id="name" placeholder="Name"></input>
-            <input type="number" name="amount" id="amount" placeholder="$$.¢¢"></input>
+            <input type="text"  placeholder="Name" ref={ input => this.buyInput = input }></input>
+            <input type="number" placeholder="$$.¢¢" ref={ input => this.costInput = input } ></input>
             <button type="button" onClick={this.adding.bind(this)}>+</button>
           </form>
         </div>
@@ -90,4 +61,16 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+    state => ({
+        currentStore: state
+    }),
+    dispatch => ({
+       onAddBuy: obj => {
+           dispatch({ type: 'ADD', buy: obj })
+       },
+       onSortStore: obj => {
+           dispatch({ type: 'SORT', buy: obj })
+       }
+    })
+)(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initState = [];
+
+const widget = (state = initState, action) => {
+    if(action.type === 'ADD'){
+        return [
+            ...state,
+            action.buy
+        ]
+    } else if(action.type === 'Sort'){
+        return [
+            action.buy
+        ]
+    } 
+    return state;
+}
+
+const store = createStore(widget);
+
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <App /> 
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
